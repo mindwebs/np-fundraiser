af38a5fe1667f27f7d655ca5f98e661e0eccec20
singhayushh <coding.ayush@gmail.com>
2021-08-28 04:38:22 +0530

    probable fix for payments

--- fundraiser.js
+++ fundraiser.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/views"));
 app.set("case sensitive routing", true);
 app.set("view engine", "ejs");
